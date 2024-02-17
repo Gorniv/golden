09f4bb67037eca3aa8b241b083d5318696d3178e
Kravchenko Igor <Gornivv@gmail.com>
2024-02-17 12:20:36 +0100

    new devices

--- lib/src/device.dart
+++ lib/src/device.dart
@@ -2,6 +2,8 @@ import 'dart:ui';
 
 import 'package:flutter/widgets.dart';
 
+/// https://iosref.com/res
+/// https://developer.apple.com/design/human-interface-guidelines/layout#Device-screen-sizes-and-orientations
 class Device {
   const Device({
     required this.name,
@@ -14,27 +16,93 @@ class Device {
   });
 
   /// [iPhone5S] matches specs of iphone5s the smallest device
+  /// iPhone SE (gen 1)
   static const Device iPhone5S = Device(
     name: 'iPhone5S',
-    size: Size(320, 568),
-    safeArea: EdgeInsets.only(top: 44, bottom: 34),
+    size: Size(640, 1136),
+    devicePixelRatio: 2,
   );
 
   /// [iphone11] matches specs of iphone11, but with lower DPI for performance
   static const Device iphone11 = Device(
     name: 'iphone11',
-    size: Size(414, 896),
-    devicePixelRatio: 1.0,
+    size: Size(828, 1792),
+    devicePixelRatio: 2.0,
     safeArea: EdgeInsets.only(top: 44, bottom: 34),
   );
 
+  static const Device iphone11Pro = Device(
+    name: 'iphone11Pro',
+    size: Size(1125, 2436),
+    devicePixelRatio: 3.0,
+    safeArea: EdgeInsets.only(top: 44, bottom: 34),
+  );
+
+  static const Device iphone11ProMax = Device(
+    name: 'iphone11ProMax',
+    size: Size(1242, 2688),
+    devicePixelRatio: 3.0,
+    safeArea: EdgeInsets.only(top: 44, bottom: 34),
+  );
+
+  static const Device iphone14 = Device(
+    name: 'iphone14',
+    size: Size(1170, 2532),
+    devicePixelRatio: 3.0,
+    safeArea: EdgeInsets.only(top: 44, bottom: 34),
+  );
+
+  static const Device iphone14Plus = Device(
+    name: 'iphone14Plus',
+    size: Size(1284, 2778),
+    devicePixelRatio: 3.0,
+    safeArea: EdgeInsets.only(top: 44, bottom: 34),
+  );
+
+  static const Device iphone15Pro = Device(
+    name: 'iphone15Pro',
+    size: Size(1179, 2556),
+    devicePixelRatio: 3.0,
+    safeArea: EdgeInsets.only(top: 44, bottom: 34),
+  );
+
+  static const Device iphone15ProMax = Device(
+    name: 'iphone15ProMax',
+    size: Size(1290, 2796),
+    devicePixelRatio: 3.0,
+    safeArea: EdgeInsets.only(top: 44, bottom: 34),
+  );
+
+  static const Device iPadPro129 = Device(
+    name: 'iPadPro12.9',
+    size: Size(2048, 2732),
+    devicePixelRatio: 2.0,
+  );
+
+  static const Device iPadPro129Landscape = Device(
+    name: 'iPadPro12.9Landscape',
+    size: Size(2732, 2048),
+    devicePixelRatio: 2.0,
+  );
+
+  /// iPad (gen 6, 5)
+  static const Device iPad = Device(
+    name: 'iPad',
+    size: Size(1536, 2048),
+    devicePixelRatio: 2.0,
+  );
+
+  static const Device iPadLandscape = Device(
+    name: 'iPadLandscape',
+    size: Size(2048, 1536),
+    devicePixelRatio: 2.0,
+  );
+
   /// [tabletLandscape] example of tablet that in landscape mode
-  static const Device tabletLandscape =
-      Device(name: 'tablet_landscape', size: Size(1366, 1024));
+  static const Device tabletLandscape = iPadPro129Landscape;
 
   /// [tabletPortrait] example of tablet that in portrait mode
-  static const Device tabletPortrait =
-      Device(name: 'tablet_portrait', size: Size(1024, 1366));
+  static const Device tabletPortrait = iPadPro129;
 
   final String name;
   final Size size;
